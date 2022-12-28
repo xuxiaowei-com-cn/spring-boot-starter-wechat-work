9371a297da932719b501aa28eaff51fd8e1e3910
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-12-28 09:15:12 +0800

    :sparkles: 反序列化时忽略不存在的字段

--- src/main/java/org/springframework/security/oauth2/server/authorization/client/InMemoryWeChatWorkWebsiteService.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/client/InMemoryWeChatWorkWebsiteService.java
@@ -21,6 +21,7 @@ package org.springframework.security.oauth2.server.authorization.client;
  */
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -278,6 +279,7 @@ public class InMemoryWeChatWorkWebsiteService implements WeChatWorkWebsiteServic
 
 		WeChatWorkWebsiteTokenResponse weChatWorkWebsiteTokenResponse;
 		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		try {
 			weChatWorkWebsiteTokenResponse = objectMapper.readValue(forObject, WeChatWorkWebsiteTokenResponse.class);
 		}
